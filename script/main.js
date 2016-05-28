b39ffef6e4a2cc8a9d65aa0d294c9ec2ea691202
Francesc <francesc.sarrate@interactius.com>
2016-05-28 17:29:40 +0200

    content

---
+++ script/main.js
@@ -0,0 +1,253 @@
+var videos = {
+    "question0": {
+        "question": "How do we find happiness, without the after-life?",
+        "image": "http://fakeimg.pl/250x100/",
+        "video1": {
+            "title": "Title 1",
+            "quote": "quote 1",
+            "key": "question1",
+            "video": "http://techslides.com/demos/sample-videos/small.mp4",
+            "concept": "concept"
+        },
+        "video2": {
+            "title": "Title 2",
+            "quote": "quote 2",
+            "key": "question2",
+            "video": "http://techslides.com/demos/sample-videos/small.mp4",
+            "concept": "concept"
+        }
+
+    },
+    "question1": {
+        "question": "Chachi question 1?",
+        "image": "http://fakeimg.pl/250x100/",
+        "video1": {
+            "title": "Title 1.1",
+            "quote": "quote 1.1",
+            "key": "question1.1",
+            "video": "http://techslides.com/demos/sample-videos/small.mp4",
+            "concept": "concept"
+        },
+        "video2": {
+            "title": "Title 1.2",
+            "quote": "quote 1.2",
+            "key": "question1.2",
+            "video": "http://techslides.com/demos/sample-videos/small.mp4",
+            "concept": "concept"
+        }
+    },
+    "question2": {
+        "question": "Chachi question 2?",
+        "image": "http://fakeimg.pl/250x100/",
+        "video1": {
+            "title": "Title 2.1",
+            "quote": "quote 2.1",
+            "key": "question2.1",
+            "video": "http://techslides.com/demos/sample-videos/small.mp4",
+            "concept": "concept"
+        },
+        "video2": {
+            "title": "Title 2.2",
+            "quote": "quote 2.2",
+            "key": "question2.2",
+            "video": "http://techslides.com/demos/sample-videos/small.mp4",
+            "concept": "concept"
+        }
+    },
+    "question1.1": {
+        "question": "Chachi question 1.1?",
+        "image": "http://fakeimg.pl/250x100/",
+        "video1": {
+            "title": "Title 1.1.1",
+            "quote": "quote 1.1.1",
+            "key": "Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing.",
+            "video": "http://techslides.com/demos/sample-videos/small.mp4",
+            "concept": "concept"
+        },
+        "video2": {
+            "title": "Title 1.2.2",
+            "quote": "quote 1.2.2",
+            "key": "Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing.",
+            "video": "http://techslides.com/demos/sample-videos/small.mp4",
+            "concept": "concept"
+        }
+    },
+    "question1.2": {
+        "question": "Chachi question 1.2?",
+        "image": "http://fakeimg.pl/250x100/",
+        "video1": {
+            "title": "Title 1.2.1",
+            "quote": "quote 1.2.1",
+            "key": "Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing.",
+            "video": "http://techslides.com/demos/sample-videos/small.mp4",
+            "concept": "concept"
+        },
+        "video2": {
+            "title": "Title 1.2.2",
+            "quote": "quote 1.2.2",
+            "key": "Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing.",
+            "video": "http://techslides.com/demos/sample-videos/small.mp4",
+            "concept": "concept"
+        }
+    },
+    "question2.1": {
+        "question": "Chachi question 2.1?",
+        "image": "http://fakeimg.pl/250x100/",
+        "video1": {
+            "title": "Title 2.1.1",
+            "quote": "quote 2.1.1",
+            "key": "Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing.",
+            "video": "http://techslides.com/demos/sample-videos/small.mp4",
+            "concept": "concept"
+        },
+        "video2": {
+            "title": "Title 2.2.2",
+            "quote": "quote 2.2.2",
+            "key": "Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing.",
+            "video": "http://techslides.com/demos/sample-videos/small.mp4",
+            "concept": "concept"
+        }
+    },
+    "question2.2": {
+        "question": "Chachi question 2.2?",
+        "image": "http://fakeimg.pl/250x100/",
+        "video1": {
+            "title": "Title 2.2.1",
+            "quote": "quote 2.2.1",
+            "key": "Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing.",
+            "video": "http://techslides.com/demos/sample-videos/small.mp4",
+            "concept": "concept"
+        },
+        "video2": {
+            "title": "Title 2.2.2",
+            "quote": "quote 2.2.2",
+            "key": "Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing.",
+            "video": "http://techslides.com/demos/sample-videos/small.mp4",
+            "concept": "concept"
+        }
+    }
+};
+var index = 2;
+
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        verticalCentered: true,
+        sectionsColor: ['#000000', '#000000', '#000000', '#000000', '#000000', '#000000', '#000000', '#000000', '#000000'],
+        afterRender: startQuest
+    });
+    $.fn.fullpage.setMouseWheelScrolling(false);
+    $.fn.fullpage.setAllowScrolling(false);
+    startWebCam();
+});
+var startWebCam = function() {
+    var video = document.querySelector("#videoElement");
+
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+
+    if (navigator.getUserMedia) {
+        navigator.getUserMedia({
+            video: true
+        }, handleVideo, videoError);
+    }
+
+    function handleVideo(stream) {
+        video.src = window.URL.createObjectURL(stream);
+    }
+
+    function videoError(e) {
+        // do something
+    }
+}
+
+var startQuest = function() {
+    $('#careta').get(0).play();
+    $('#careta').on('ended', function() {
+        startQuestion("question0");
+
+    });
+
+}
+var startQuestion = function(questionKey) {
+    if (questionKey.indexOf("question") == 0) {
+        var question = videos[questionKey];
+        $("#question").removeAttr("style");
+        $("#question>h1").text(question.question);
+        $.fn.fullpage.moveTo(index, index);
+        $("#question").addClass("animated flipInX fixedElement");
+        $("#question").removeClass("hidden");
+        setTimeout(function() {
+            startVideos()
+        }, 3000);
+        $("#section" + index).css('background-image', 'url(' + question.image + ')');
+
+        index++;
+        var template = $("#template").html();
+        var $template = $("#section" + index).find(".fp-tableCell").append(template);
+        var $container1 = $template.find(".container1");
+        var $container2 = $template.find(".container2");
+        fillContainer($container1, question.video1);
+        fillContainer($container2, question.video2);
+    } else {
+        startEnd(questionKey);
+    }
+}
+var startEnd = function(key) {
+    $("#question").addClass("hidden");
+    $("#last").fadeIn(1000, function() {
+        $.fn.fullpage.moveTo(index, index);
+        $("#videoElement").removeClass("hidden");
+        type(key, $("#lastTitle"), function() {
+            setTimeout(function() {
+                $("#last").fadeOut(1000);
+                setTimeout(function() {
+                    $("#credits").fadeIn(1000);
+                }, 5000);
+            }, 2000);
+
+        });
+    });
+}
+
+function type(string, element, callback) {
+    (function writer(i, display) {
+        if (i++ >= string.length) {
+            callback();
+        }
+
+        element.html(string.substring(0, i));
+        var rand = Math.floor(Math.random() * (100));
+        setTimeout(function() {
+            writer(i);
+        }, rand);
+    })(0)
+}
+var fillContainer = function($container, video) {
+    $container.find(".title").text(video.title);
+    $container.find(".quote").text(video.quote);
+    $container.find("video source").attr('src', video.video).load();
+    $container.find("button").click(function(event) {
+        startQuestion(video.key);
+    });
+    $container.find("button").text(video.concept);
+}
+var startVideos = function() {
+    $.fn.fullpage.moveTo(index, index);
+    $("#question").removeClass("flipInX");
+    $("#question").animate({
+        fontSize: "8px",
+        top: "30px"
+    }, 500);
+    var array = $("#section" + index).find('video');
+    for (var i = 0; i < array.length; i++) {
+        var video = array.get(i);
+        video.play();
+        var $video = $(video);
+        addVideoButton($video);
+    }
+    index++;
+}
+var addVideoButton = function($video) {
+    $video.on('ended', function() {
+        $video.parent().parent().find('.video-button').removeClass('hidden');
+    });
+}
